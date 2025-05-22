7c072520695b1163075f8cac0b7fbcc183e7f260
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-05-23 00:27:24 +0530

    add CreateFileDto

--- server/src/files/dto/create-file.dto.ts
+++ server/src/files/dto/create-file.dto.ts
@@ -1,1 +1,13 @@
-export class CreateFileDto {}
+export class CreateFileDto {
+  originalName: string;
+  filename: string;
+  filePath: string;
+  checksum: string;
+  mimeType: string;
+  fileSize: number;
+  width?: number | null;
+  height?: number | null;
+  title?: string | null;
+  description?: string | null;
+  tags?: string[] | null;
+}
